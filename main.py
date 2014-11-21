6c137e71cfcbbe3e515e9bdda63f2b6a6bf3cebf
Yangjie Li(懒) <latyas@gmail.com>
2014-11-21 11:45:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import multiprocessing
+import drcom
+import eapauth
+
+user_info = {'username': "",
+             'password': "",
+             'ethernet_interface': "eth0",
+             'daemon': False,
+             'dhcp_command': "dhclient eth0"
+             }
+
+# 802.1x成功以后就进入正常drcom认证
+auth = eapauth.EAPAuth(login_info, success_callback=drcom.main)
+auth.serve_forever()
